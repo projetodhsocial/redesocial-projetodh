cd3e1ff2571692fbe7a5e356c599286e5fc81e5f
Adonis Cipriano <desenvolvimento777@gmail.com>
2020-03-23 23:57:15 -0300

    Novo menu

---
+++ inc/head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $titulo_pagina;?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,900&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="./assets/css/style_menu.css">
+</head>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php $titulo_pagina = "Projeto Social"; ?>
+<?php require_once('./inc/head.php') ?>
+<?php require_once('./inc/header.php') ?>
+
+
+
+<?php require_once('./inc/footer.php') ?>
