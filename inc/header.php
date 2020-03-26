eadcce107ac0988fb5dcec3032954330c9ea3b22
Adonis Cipriano <desenolvimento777@gmial.com>
2020-03-26 20:13:10 -0300

    Paginas linkadas - refotorando o CSS

--- inc/header.php
+++ inc/header.php
@@ -8,7 +8,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="#./index.php">Início</a>
+                        <a class="nav-link" href="./index.php">Início</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Sobre</a>
@@ -17,7 +17,7 @@
                         <a class="nav-link" href="#">Contato</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Cadastro</a>
+                        <a class="nav-link" href="./cadastro.php">Cadastro</a>
                     </li>
                 </ul>
             </div>
--- index.php
+++ index.php
@@ -3,25 +3,9 @@
 <?php require_once('./inc/header.php') ?>
 
 <div class="container-fluid">
-    <img src="./assets/img/splash.png" class="rounded float-left" alt="Imagem balão de mensagem">
+  <img src="./assets/img/foto-conexao-projetodh.png" class="img-fluid" alt="Imagem responsiva">
 
-    <div class="col">
-        <form>
-          <div class="form-group">
-            <label for="exampleInputEmail1">Endereço de email</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email">
-            <small id="emailHelp" class="form-text text-muted">Nunca vamos compartilhar seu email, com ninguém.</small>
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Senha</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
-          </div>
-          <!-- <div class="form-group form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Clique em mim</label>
-          </div> -->
-          <button type="submit" class="btn">Enviar</button>
-        </form>
+    
   
 </div>
 
