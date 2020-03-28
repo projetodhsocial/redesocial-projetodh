4c284eaf55bae6b00dbd15c3466dc81db392a57f
Adonis Cipriano <desenolvimento777@gmial.com>
2020-03-28 01:47:15 -0300

    tela 404 - not found

---
+++ 404notfound.php
@@ -0,0 +1,11 @@
+<?php require_once("./inc/head.php"); ?>
+<?php require_once("./inc/header.php"); ?>
+
+<div class="container-fluid  d-flex flex-column justify-content-center container-404">
+    <img  class="rounded mx-auto d-block mb-3" src="./assets/img/error.png" alt="Imagme de erro">
+    <h2 class="text-center">Oooops! Page note found.</h2>
+    <h1 class="text-center">404</h1>
+    <p class="text-center">We can't find the page you're looking for.</p>
+</div>
+
+<?php require_once("./inc/footer.php"); ?>
